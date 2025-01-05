826748fd97efea64b3556fbfdfb9e612e956e9d6
Dilshod2010 <dilshodjon.fahriddinov@gmail.com>
2025-01-05 16:35:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+//const maxbalance = 80
+//const mega = +prompt("O'qishga kirish bali 80 ")
+
+//if (maxbalance <81) {
+    //console.log("O'qishga kirdingiz");
+    
+//}else{
+   // console.log("O'qishga kirmadingiz");''
+    
+//}
+
+const Maxball = 80;
+const myball = +prompt("Please enter your Ball")
+
+if (Maxball <= myball ) {
+    alertI("O'qishga kirdingiz")
+}else{
+    alert("O'qishga kirmadingiz")
+}
